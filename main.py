5ac18ae2a551d1a4ff328ae2dfb5964b4937af93
Ladan <ladan.rb@gmail.com>
2023-03-28 14:59:29 +0200

    "Update with Exceptions and Update the Dictionary with Special Characters"

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from morse_code_chart import morse_code_dict
 
 message = input('What do you want me to transalte into the Morse code for you?')
 decoded_message = ''
+
 for character in message:
     try:
         decoded_message += morse_code_dict[character.upper()]+' '
     except KeyError:
         decoded_message += morse_code_dict[character]+' '
+
 print(decoded_message)
